ee64631ec9015b2cd9a98a72609ed1dedd0b23ef
ronischiaretti <pnamaquina@gmail.com>
2025-01-31 17:09:25 -0300

    arrumando o endereço da minha api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+// Defina a URL do seu backend hospedado na Vercel
 const api = axios.create({
-   baseURL:' http://localhost:3000'
-})
+   baseURL: "https://bdsaulo.vercel.app/",
+   headers: {
+     "Content-Type": "application/json",
+   },
+ });
 
 export default api 
