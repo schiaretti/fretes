3bff83c09ef84017a71a20fa873ea9479a9b450a
ronischiaretti <pnamaquina@gmail.com>
2025-01-31 19:07:26 -0300

    tirei o jwt

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bdsaulo-j273amk7f-schiarettis-projects.vercel.app/",
-});
-
-// Interceptador para adicionar o token JWT automaticamente
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // Pegue o token do localStorage
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`; // Envia o token no cabeçalho
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
+  baseURL: "https://bdsaulo-j273amk7f-schiarettis-projects.vercel.app/", // URL do backend
 });
 
 export default api;
+
