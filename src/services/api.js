4df095ffefa98ecdd0ec08214add2352f59ef507
ronischiaretti <pnamaquina@gmail.com>
2025-01-31 19:53:48 -0300

    troquei a url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-   baseURL:' mongodb+srv://saulo:<db_password>@saulodb.cekl7.mongodb.net/?retryWrites=true&w=majority&appName=saulodb'
+   baseURL:'https://bdsaulo-lmecnbdv9-schiarettis-projects.vercel.app/'
 })
 
 export default api 
