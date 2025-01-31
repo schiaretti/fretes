d2f2255ded564346dce773ab0d50fcbb9a0e0820
ronischiaretti <pnamaquina@gmail.com>
2025-01-31 17:46:05 -0300

    arrumando

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,16 @@
-import axios from 'axios'
+import axios from "axios";
 
-// Defina a URL do seu backend hospedado na Vercel
 const api = axios.create({
-   baseURL: "https://bdsaulo.vercel.app/",
-   headers: {
-     "Content-Type": "application/json",
-   },
- });
+  baseURL: "https://bdsaulo-8ekb874h0-schiarettis-projects.vercel.app",
+});
 
- // Interceptador para incluir o token automaticamente
+// Interceptador para incluir o token automaticamente
 api.interceptors.request.use((config) => {
-   const token = localStorage.getItem("token");
-   if (token) {
-     config.headers.Authorization = `Bearer ${token}`;
-   }
-   return config;
- });
- 
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-export default api 
+export default api;
