766539c68a8e251a9b59345d7504d5c1d1b04c0c
ronischiaretti <pnamaquina@gmail.com>
2025-01-31 18:16:24 -0300

    arrumando

--- src/services/api.js
+++ src/services/api.js
@@ -4,13 +4,15 @@ const api = axios.create({
   baseURL: "https://bdsaulo-8ekb874h0-schiarettis-projects.vercel.app",
 });
 
-// Interceptador para incluir o token automaticamente
+// Interceptador para adicionar o token JWT automaticamente
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token"); // Pegue o token do localStorage
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`; // Envia o token no cabeçalho
   }
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
